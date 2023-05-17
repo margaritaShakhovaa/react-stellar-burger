1e82f663c8d6114c3d28f5f630dd6cbebb45ad1c
Margarita <alis-weasley@mail.ru>
2023-05-17 22:21:14 +0300

    сверстать app-header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -5,13 +5,13 @@ import styles from './app-header.module.css';
 class AppHeader extends React.Component {
   render() {
     return (
-        <header className={styles.app_header}>
+        <header className={styles.header}>
           <div className={styles.menu}>
-            <div className={`pt-4 pr-5 pb-4 pl-4 ${styles.menu_item}`}>
+            <div className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
               <BurgerIcon type="primary" />
               <p className="text text_type_main-small">Конструктор</p>
             </div>
-            <div className={styles.menu_item}>
+            <div className={`pt-4 pr-5 pb-4 pl-5 ${styles.menu_item}`}>
               <ListIcon type="secondary" />
               <p className="text text_type_main-small text_color_inactive">Лента заказов</p>
             </div>
@@ -20,7 +20,7 @@ class AppHeader extends React.Component {
             <Logo />
           </div>
           <div>
-            <div className={styles.menu_item}>
+            <div className={`pt-4 pr-5 pb-4 pl-4 ${styles.menu_item}`}>
               <ProfileIcon type="secondary" />
               <p className="text text_type_main-small text_color_inactive">Личный кабинет</p>
             </div>
