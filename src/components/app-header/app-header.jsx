5843b5efad37f5c4520eee6970f6976095a4940d
Margarita <alis-weasley@mail.ru>
2023-05-17 13:16:52 +0300

    сверстать app-header

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+        <header className={styles.app_header}>
+          <div className={styles.menu}>
+            <div className={`pt-4 pr-5 pb-4 pl-4 ${styles.menu_item}`}>
+              <BurgerIcon type="primary" />
+              <p className="text text_type_main-small">Конструктор</p>
+            </div>
+            <div className={styles.menu_item}>
+              <ListIcon type="secondary" />
+              <p className="text text_type_main-small text_color_inactive">Лента заказов</p>
+            </div>
+          </div>
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+          <div>
+            <div className={styles.menu_item}>
+              <ProfileIcon type="secondary" />
+              <p className="text text_type_main-small text_color_inactive">Личный кабинет</p>
+            </div>
+          </div>
+        </header>
+    )
+  }
+}
+
+export default AppHeader;
