5b8d8ae23bbbc4d34b0a89866eedf13653197ece
Margarita <alis-weasley@mail.ru>
2023-05-29 11:23:09 +0300

    feat: подключиться к API

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,45 @@
+import React from "react";
 import styles from "./app.module.css";
-import { data } from "../../utils/data";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
+const apiUrl = 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
+
+  const [ingredients, setIngredients] = React.useState([] );
+
+  React.useEffect(() => {
+    getIngredientData();
+  }, []);
+
+  const getIngredientData = () => {
+    fetch(apiUrl)
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Ошибка ${res.status}`);
+        })
+        .then(res => setIngredients(res.data))
+        .catch(error => console.log(error))
+  }
+
+  const data = ingredients;
+
   return (
-    <div className={styles.app}>
-      <AppHeader />
-      <main className={`mt-10 mb-10 ${styles.main}`}>
-        <BurgerIngredients data={data}/>
-        <BurgerConstructor data={data}/>
-      </main>
-    </div>
+      <div className={styles.app}>
+        <AppHeader />
+        <main className={`mt-10 mb-10 ${styles.main}`}>
+          { data.length > 0 &&
+              <>
+                <BurgerIngredients data={data} />
+                <BurgerConstructor data={data} />
+              </>
+          }
+        </main>
+      </div>
   );
 }
 
